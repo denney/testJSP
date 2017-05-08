bef5db8d9fa71efcabe59b5c09654c16a399a9c9
denney <695493589@qq.com>
2017-05-08 16:29:50 +0800

    Initial commit

---
+++ src/main/java/com/example/TestJspApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJspApplication.class, args);
+	}
+}
