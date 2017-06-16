b91fedb994dab3c750ed81d0d7e267f8a64bffef
dst <695493589@qq.com>
2017-06-17 05:19:43 +0800

    idea 提交

--- src/main/java/com/example/A.java
+++ src/main/java/com/example/A.java
@@ -1,8 +1,1 @@
-package com.example;
-
-/**
- * Created by admin on 2017/6/17.
- */
-public class A {
-    //idea
-}
+package com.example;/** * Created by admin on 2017/6/17. */public class A {    //idea}
