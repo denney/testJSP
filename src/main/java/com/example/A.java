a6fe0654078e12fdfd4fffa30d34fbfb0ab534da
dst <695493589@qq.com>
2017-06-18 02:29:20 +0800

    新建分支haha，并修改A类，并提交

--- src/main/java/com/example/A.java
+++ src/main/java/com/example/A.java
@@ -1,58 +1,1 @@
-package com.example;
-
-public class A {
-//来自于eclipse222000000
-
-//    000
-
-
-//    9898
-//	eclipse
-	
-	//eclipse  lalala l
-
-
-    //wocao
-    //idea laile
-//	eclpse
-
-//sddssd
-    //qqq
-	//222
-
-//	sdsd
-	//2222
-
-//    ss
-
-
-
-//    idea 695493589
-
-
-
-
-//	69549
-
-
-//	eclipse
-
-
-//    23232
-
-//    3434
-
-//	22
-//    dsflkjdslfdjs
-
-
-
-
-    //idea =======idea
-
-//1111111111111111
-
-    //idea =======idea22222222
-
-
-}
+package com.example;public class A {//来自于eclipse222000000//    000//    9898//	eclipse		//eclipse  lalala l    //wocao    //idea laile//	eclpse//sddssd    //qqq	//222//	sdsd	//2222//    ss//    idea 695493589//	69549//	eclipse//    23232//    3434//	22//    dsflkjdslfdjs    //idea =======idea//1111111111111111    //idea =======idea22222222//   新建分支haha}
